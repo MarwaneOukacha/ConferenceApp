3950e3e49fa53d2cc88fa8b6c0a215bec06267a7
Marwane OUKACHA <oukachamarwane0@gmail.com>
2025-09-15 16:37:32 +0000

    SetUp project structure

---
+++ my-spring-app/src/main/java/com/conf/App.java
@@ -0,0 +1,13 @@
+package com.conf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
